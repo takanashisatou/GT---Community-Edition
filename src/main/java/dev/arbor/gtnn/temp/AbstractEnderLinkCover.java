9ce41ea3af24784fac48c02606b78a66f02404b7
moguang <184676648@qq.com>
2025-03-11 08:05:33 +0800

    update for gtm 1.6.4, fix the enderlinkCover

--- src/main/java/dev/arbor/gtnn/temp/AbstractEnderLinkCover.java
+++ src/main/java/dev/arbor/gtnn/temp/AbstractEnderLinkCover.java
@@ -163,7 +163,7 @@ public abstract class AbstractEnderLinkCover<T extends VirtualEntry> extends Cov
         if (permission == Permissions.PUBLIC)
             return null;
         else if (owner instanceof PlayerOwner playerOwner)
-            return playerOwner.getPlayerUUID();
+            return playerOwner.getUUID();
         else if (owner instanceof ArgonautsOwner argonautsOwner)
             return argonautsOwner.getPlayerUUID();
         else if (owner instanceof FTBOwner ftbOwner)
