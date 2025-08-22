8f3b564191700d026162a89adb7627cd75a11c38
Arborsm <BlueGrayx@qq.com>
2025-08-22 19:41:38 +0800

    and more

--- src/main/java/dev/arbor/gtnn/mixin/gt/GTRecipeWidgetMixin.java
+++ src/main/java/dev/arbor/gtnn/mixin/gt/GTRecipeWidgetMixin.java
@@ -3,8 +3,10 @@ package dev.arbor.gtnn.mixin.gt;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.integration.xei.widgets.GTRecipeWidget;
-import dev.arbor.gtnn.client.gui.VoltageBorderWidget;
+
 import net.minecraft.ChatFormatting;
+
+import dev.arbor.gtnn.client.gui.VoltageBorderWidget;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
